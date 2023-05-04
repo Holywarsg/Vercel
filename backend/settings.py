0d850789c3bffee393e7302f0c6f25d9b39baf3e
Holywarsg <89921707+Holywarsg@users.noreply.github.com>
2023-05-04 19:17:56 +0300

    Update settings.py
    vgvvbnv

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-7nj9-=g3192f!_i_l(3pxuhiy#6xza*)gjcrt8)_z#4f2my+mu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
